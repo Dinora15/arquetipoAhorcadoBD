24986094954657cd292399fafd5800dda0cb9bd9
Dinora15 <108556884+Dinora15@users.noreply.github.com>
2023-04-20 15:02:45 -0600

    Add files via upload

--- src/Drive/Drive.java
+++ src/Drive/Drive.java
@@ -1,1 +1,15 @@
+package Drive;
 
+import arquetipoAh.Menu;
+
+public class Drive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Menu seleccion=new Menu();
+		seleccion.Inicio();
+
+	}
+
+}
