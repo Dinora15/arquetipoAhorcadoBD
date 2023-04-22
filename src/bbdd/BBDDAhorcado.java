d9867615fcdbd0d390d755b4b8fe4872d63e79a9
Dinora15 <108556884+Dinora15@users.noreply.github.com>
2023-04-22 14:57:22 -0600

    Update BBDDAhorcado.java

--- src/bbdd/BBDDAhorcado.java
+++ src/bbdd/BBDDAhorcado.java
@@ -1,15 +1,23 @@
 package bbdd;
 import arquetipoAhorcadoBD.*;
 
-
+public class BBDDAhorcado {
+	
+	 
+	    private String servidor;
+	    private String usuario;
+	    private String pass; 
+	    private String bbdd;
+	    
 	    
 
 	    public BBDDAhorcado(){
 	        
-	        this.servidor = "jdbc:mysql://localhost:3306/ahorcadobd"; //modificar
+	        this.servidor = "jdbc:mysql://localhost:3306"; 
 	        this.bbdd = "ahorcadobd"; 
 	        this.usuario = "root";
 	        this.pass = ""; 
+		    
 
 	        try {
 	            Class.forName("com.mysql.jdbc.Driver"); 
