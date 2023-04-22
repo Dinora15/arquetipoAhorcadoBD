3fe94bb63aff86dcca2dfd0f64ea80c5a3558511
Dinora15 <108556884+Dinora15@users.noreply.github.com>
2023-04-22 14:55:15 -0600

    Update BBDDAhorcado.java

--- src/bbdd/BBDDAhorcado.java
+++ src/bbdd/BBDDAhorcado.java
@@ -1,1 +1,22 @@
+package bbdd;
+import arquetipoAhorcadoBD.*;
 
+
+	    
+
+	    public BBDDAhorcado(){
+	        
+	        this.servidor = "jdbc:mysql://localhost:3306/ahorcadobd"; //modificar
+	        this.bbdd = "ahorcadobd"; 
+	        this.usuario = "root";
+	        this.pass = ""; 
+
+	        try {
+	            Class.forName("com.mysql.jdbc.Driver"); 
+	        } catch (Exception e) {
+	            throw new AhorcadoException("Driver de BD no localizado"+e);
+	        }
+	    }
+	    
+		
+}
